eb6c1b1b7f0a8bd254df448dc2a876fa29ab44ac
Thomas Vargas <rodrigues.pauloricardo@gmail.com>
2021-12-19 10:42:00 -0300

    att

--- index.js
+++ index.js
@@ -5,12 +5,13 @@ function verificar( nome, quantidade ) {
 		
     saida = " <br> Você Pediu... <br>";
     
+    
     // itera baseado na quantidade de elementos
     for ( i = 0; i < quantidade; i++ ) {
 
         // obtém cada elemento pelo id
         checkBox = document.getElementById( nome + ( i + 1 ) );
-        if (checkBox.value !== ""){
+        if (checkBox.value === ""){
             alert = ("jkhjh")
         }
         // se o checkbox estiver marcado, adiciona mais uma linha na string de saida.
@@ -21,3 +22,5 @@ function verificar( nome, quantidade ) {
     // mostra a saída
     resposta.innerHTML = (saida + "<br> Seu pedido está em preparação.....");
 }
+
+/*hghkjkjk
