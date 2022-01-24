746a94c58145393fe7480459578ab7cb415b21e5
Thomas Vargas <rodrigues.pauloricardo@gmail.com>
2022-01-24 16:46:25 -0300

    at

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
 var resposta = document.getElementById('res') ;
 
-
-
-
-function verificar( nome, quantidade ) {
-
-
-    document.getElementById('res').style.background = 'white';
+function verificar( nome, quantidade ) {  
+     document.getElementById('res').style.background = 'white';
      document.getElementById('res').style.background = 'white';
      document.getElementById
      ('res').style.bottom = '18%';
-
      res.style.height = '60%';
 
-
-    let el = document.getElementById('closed');
-
-   
+    let el = document.getElementById('closed');   
     el.style.cssText = 
     'color: black;'+
     'background-color: rgb(247, 155, 36);'+
@@ -25,45 +16,43 @@ function verificar( nome, quantidade ) {
     'padding: 15px;'
     
    
-    saida = " <br> Você Pediu... <br>"; 
+    saida = " <br> Você Pediu... <br>";
+    
 
     // itera baseado na quantidade de elementos
     for ( i = 0; i < quantidade; i++ ) {
            
         // obtém cada elemento pelo id
    checkBox = document.getElementById( nome + ( i + 1 ) );
-   //  if (checkBox.value !== ""){
-  //        alert  ("Escolha um ítem")      
+
+    // if (checkBox.checked === false){
+    //      alert  ("Escolha um ítem")      
     // reseta()
- //     }
-        
-        // se o checkbox estiver marcado, adiciona mais uma linha na string de saida.
+    // }
 
-        const batata = {
-          valor: 25.00
-        }
-        
-
-
-        if (checkBox.checked && checkBox.value === 'Batata Frita') {    
-            saida +="\n <br>" + checkBox.value + batata.valor;
-         
-        }  
-      }
-     
-       
-    // mostra a saída
-    resposta.innerHTML = (saida +  "<br><br> Seu pedido está em preparação....." );
     
+     
+     
+  
+           
+        // se o checkbox estiver marcado, adiciona mais uma linha na string de saida.
+        if (checkBox.checked) { 
+
+                
+
+            saida +="\n <br>" + checkBox.value;                  
+                 
+        } 
+      }
+            
+    // mostra a saída
+    resposta.innerHTML = (saida +  "<br><br> Seu pedido está em preparação....." );    
 }
 
 function fechar(){
         document.getElementById("res").style.display = "none";
         document.getElementById("closed").style.display = "none";
         window.location.reload();
-
-     
-
 
 }
 
